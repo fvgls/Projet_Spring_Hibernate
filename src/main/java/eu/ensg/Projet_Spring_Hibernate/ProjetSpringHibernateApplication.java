e2cb0d524a68919f461de20b347c90856daaee7e
fvgls <vignollesfanny@gmail.com>
2020-02-03 11:29:52 +0100

    Second try: better initialisation

---
+++ src/main/java/eu/ensg/Projet_Spring_Hibernate/ProjetSpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package eu.ensg.Projet_Spring_Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSpringHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/ensg/Projet_Spring_Hibernate/ProjetSpringHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ensg.Projet_Spring_Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
